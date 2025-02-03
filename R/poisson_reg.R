ce9b70ab85200ce742ea1453800389bd4cb8884e
lyvqian <117559465+lyvqian@users.noreply.github.com>
2025-02-03 09:35:26 -0500

    Update poisson_reg.R

--- R/poisson_reg.R
+++ R/poisson_reg.R
@@ -1,18 +1,19 @@
 poisson_reg <- function(X, Y, tol=1e-5, iter_max=25){
   q=ncol(X)
-  beta=rep(0,q)
+  X <- cbind(rep(1,nrow(X)), X)
+  beta=rep(0,q+1)
   iter=0
-  while (epsilon > tol & ite <= ite_max){
+  epsilon=99
+  while (epsilon > tol & iter <= iter_max){
     eta = X %*% beta
     mu = exp(eta)
     nu = exp(eta)
     V = diag(as.vector(nu))
     y_star = eta + solve(V)  %*% (Y-mu)
-    beta_new = solve(X %*% V %*% t(X)) %*% t(X) %*% V %*% y_star
+    beta_new = solve(t(X) %*% V %*% X) %*% t(X) %*% V %*% y_star
     epsilon = sqrt(t(beta_new-beta)%*%(beta_new-beta))
     beta = beta_new
     iter = iter + 1
   } 
   return(beta)
 }
-
